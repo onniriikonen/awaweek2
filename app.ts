c74f6aa66dce1228e9fed44d66b22b299f8b542c
enkikkeli <onni.riikonen@student.lut.fi>
2024-11-11 22:22:41 +0200

    2

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
 console.log("Hello World!");
+
+type TVehicle = {
+    model: string;
+    color: string;
+    year: number;
+    power: number;
+};
+
+const vehicle: TVehicle = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60
+};
+
+console.log(vehicle);
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
 console.log("Hello World!");
+const vehicle = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60
+};
+console.log(vehicle);
